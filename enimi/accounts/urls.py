cc5897e0f11db06f6baf2cd456f194dbb04d38f5
Zhuldyz Kalbayeva <kalbayevazh@gmail.com>
2022-12-11 17:00:08 +0600

    #2 Создала новую индексную страницу
    Создали индексную страницу index.html, создан шаблон navbar c логтипом который является ссылкой на главную страницу и создана кнопка при нажатии на которую появляется выпадающий список с типами пользователей доступными для регистрации

--- enimi/accounts/urls.py
+++ enimi/accounts/urls.py
@@ -2,9 +2,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from accounts.views.example_view import ExampleView
+from accounts.views import IndexView
 
 urlpatterns = [
-    path('', ExampleView.as_view(), name='login'),
+    path('', IndexView.as_view(), name='index'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
               + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- enimi/accounts/views/example_view.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.views.generic import TemplateView
-
-
-class ExampleView(TemplateView):
-    template_name = 'example_index.html'
