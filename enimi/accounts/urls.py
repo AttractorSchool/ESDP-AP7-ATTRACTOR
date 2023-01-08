894866e22661f785c268c3bec1763f01ca987ab4
Zhuldyz Kalbayeva <kalbayevazh@gmail.com>
2023-01-08 23:53:53 +0600

    Разработка календаря у Репетитора

--- enimi/accounts/urls.py
+++ enimi/accounts/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from accounts.views.accounts import AccountCreateView, logout_view, LoginView,PasswordChangeView, UserUpdateView, UserWithoutEmailUpdateView
-from accounts.views.tutor_modules import TutorModuleCreateView
-
 from accounts.views.add_child import AddChildView
 
 urlpatterns = [
@@ -9,7 +7,6 @@ urlpatterns = [
     path('register/account/<int:pk>/change-password/', PasswordChangeView.as_view(), name='change_password'),
     path('logout/', logout_view, name='logout'),
     path('login/', LoginView.as_view(), name='login'),
-    path('register/tutor/<int:pk>/', TutorModuleCreateView.as_view(), name='tutor_module_creation'),
     path('register/child/<int:pk>/', AddChildView.as_view(), name='add_child'),
     path('profile/<int:pk>/user_update', UserUpdateView.as_view(), name='user_update'),
     path('profile/<int:pk>/user_update_without_email', UserWithoutEmailUpdateView.as_view(), name='user_update_without_email'),
