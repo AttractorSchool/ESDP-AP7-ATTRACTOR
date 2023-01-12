9df05e8d9e06bd0a610e8418f7cb75c918811a32
tanitaDev <tanita.pak@gmail.com>
2023-01-12 18:46:34 +0600

    Доска студентов #40 - создала шаблон для доски студентов - вывела некоторую информацию доски студентов

--- enimi/enimi/urls.py
+++ enimi/enimi/urls.py
@@ -18,7 +18,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from board_tutors_students.views.base import BoardTutorView
+from board_tutors_students.views.base import BoardTutorView, BoardStudentView
 from enimi.views import IndexView
 
 urlpatterns = [
@@ -30,6 +30,7 @@ urlpatterns = [
       path('cabinet_tutors/', include('cabinet_tutors.urls')),
       path('schedule/', include("calendarapp.urls")),
       path('board_tutors_students/', include('board_tutors_students.urls')),
+      path('board_student', BoardStudentView.as_view(), name='board_student'),
   ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
