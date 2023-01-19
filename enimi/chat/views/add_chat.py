0e2a62abe6d8f065fe0a05548af0bed0159982f5
Rashid Gulamov <rashidgulamov7@gmail.com>
2023-01-20 02:00:05 +0600

    Изменения во воьюшке add_chat. Используется модель респонс, т.к. через нее можно вытащить все сообщения конкретно этого респонса. Удалена передача дополнительного контекста в виде сообщений и формы (классовой формы достаточно). Соответственно, изменения коснулись шаблона, значительно облегчен доступ к сообщениям через моедль респонс.

--- enimi/chat/views/add_chat.py
+++ enimi/chat/views/add_chat.py
@@ -1,27 +1,18 @@
 from django.shortcuts import redirect, get_object_or_404
-from django.views.generic import CreateView, DetailView, View
+from django.views.generic import CreateView, DetailView, View, UpdateView
 from chat.forms import ChatForm
 from chat.models import Chat
 from responses.models import Response
 
 
-class ResponsesAddChatMessageView(CreateView):
-    model = Chat
+class ResponsesAddChatMessageView(UpdateView):
+    model = Response
     template_name = 'response_chat.html'
     form_class = ChatForm
 
     def post(self, request, *args, **kwargs):
-        print(request.POST)
-        print(kwargs['pk'])
         message = request.POST['message']
         response = Response.objects.get(pk=kwargs['pk'])
         author = self.request.user
         Chat.objects.create(response=response, message=message, author=author)
         return redirect('add_chat_message', pk=kwargs['pk'])
-
-    def get_context_data(self, **kwargs):
-        print("JKKKKKK")
-        context = super().get_context_data(**kwargs)
-        context['form'] = ChatForm()
-        context['chats'] = Chat.objects.filter(response_id=self.kwargs['pk'])
-        return context
