3fdc86f96d79317fb36da3e33b5f7ad373ca5a37
Nika <nurilaassyk1994@gmail.com>
2023-01-19 17:20:04 +0600

    #30 Реализовать функционал сброса пароля посредством подтверждения email

--- enimi/enimi/urls.py
+++ enimi/enimi/urls.py
@@ -33,6 +33,6 @@ urlpatterns = [
                   path('board_tutors_students/', include('board_tutors_students.urls')),
                   path('responses/', include("responses.urls")),
                   path('chats/', include("chat.urls")),
+
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
               + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
