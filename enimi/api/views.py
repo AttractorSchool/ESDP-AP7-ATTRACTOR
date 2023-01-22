fa278b34381612952cf81e25c547febbec1208ad
Rashid Gulamov <rashidgulamov7@gmail.com>
2023-01-23 02:29:06 +0600

    Доработка логики отображения данных собесдников в чатах, а также логика добавления новых уведомлений

--- enimi/api/views.py
+++ enimi/api/views.py
@@ -16,16 +16,46 @@ class AddMessage(APIView):
         response = Response.objects.get(pk=kwargs.get('pk'))
         author = request.user
         Chat.objects.create(response=response, message=message, author=author)
-        user = author
-        if response.survey and response.survey.user.parent and response.survey.user.parent != user:
-            chats(response.survey.user.parent, user)
-        if response.survey and not response.survey.user.parent and response.survey != user:
-            chats(response.survey.user, user)
-        if response.cabinet_tutor and response.cabinet_tutor.user == user and response.author.parent:
-            chats(response.author.parent, user)
-        if response.cabinet_tutor and response.cabinet_tutor.user == user and not response.author.parent:
-            chats(response.author, user)
+        # Когда репетитор отправляет сообщение ученику, при этом сообщение приходит родителю (автор отклика - репетитор)
+        if author.type == 'tutor' and author == response.author and response.survey and response.survey.user.parent:
+            chats(response.survey.user.parent, author)
+        # Когда репетитор отправляет сообщение самостоятельному ученику (автор отклика - репетитор)
+        if author.type == 'tutor' and author == response.author and response.survey and not response.survey.user.parent:
+            chats(response.survey.user, author)
+        # Когда репетитор отправляет сообщение ученику, при этом сообщение приходит родителю (автор отклика - ученик)
+        if author.type == 'tutor' and author != response.author and response.author.parent:
+            chats(response.author.parent, author)
+        # Когда репетитор отправляет сообщение самостоятельному ученику (автор отклика - ученик)
+        if author.type == 'tutor' and author != response.author and not response.author.parent:
+            chats(response.author, author)
+        # Когда самостоятельный ученик отправляет сообщение репетитору (автор отклика - репетитор)
+        if author.type == 'student' and author != response.author:
+            chats(response.author, author)
+        # Когда самостоятльный ученик отправляет сообщение репетитору (автор отклика - ученик)
+        if author.type == 'student' and response.cabinet_tutor:
+            chats(response.cabinet_tutor.user, author)
+        # Когда ученик отправляет сообщение репетитору от лица родителя (автор отклика - репетитор)
+        if author.type == 'parents' and not response.cabinet_tutor:
+            chats(response.author, response.survey.user)
+        # Когда ученик отправляет сообщение репетитору от лица родителя (автор отклика - ученик)
+        if author.type == 'parents' and response.cabinet_tutor:
+            chats(response.cabinet_tutor.user, response.author)
         return JsonResponse({'chats': 'Сообщение добавлено'})
+
+        # user = author
+        # # Когда репетитор отправляет сообщение ученику, при этом сообщение приходит родителю (автор отклика - рпетитор)
+        # if response.survey and response.survey.user.parent and response.survey.user.parent != user:
+        #     chats(response.survey.user.parent, user)
+        # # Когда репетитор отправляет сообщение ученику, при этом сообщение приходит родителю (автор отклика - ученик)
+        # if response.cabinet_tutor and response.cabinet_tutor.user == user and response.author.parent:
+        #     chats(response.author.parent, user)
+        # # Когда репетитор отправляет сообщение самостоятельному ученику (автор отклика - репетитор)
+        # if response.survey and not response.survey.user.parent and response.survey != user:
+        #     chats(response.survey.user, user)
+        # # Когда репетитор отправляет сообщение самостоятельному ученику (автор отклика - ученик)
+        # if response.cabinet_tutor and response.cabinet_tutor.user == user and not response.author.parent:
+        #     chats(response.author, user)
+        # return JsonResponse({'chats': 'Сообщение добавлено'})
 
 
 class GetChat(View):
--- enimi/cabinet_tutors/views/responses.py
+++ enimi/cabinet_tutors/views/responses.py
@@ -6,15 +6,16 @@ from cabinet_tutors.models import TutorCabinets
 from responses.models import Response
 
 
-class MyResponsesOnStudentsView(LoginRequiredMixin, ListView):
+class OnStudentsFromTutorView(LoginRequiredMixin, ListView):
     template_name = 'my_responses_on_student.html'
     model = Response
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        context = super(MyResponsesOnStudentsView, self).get_context_data(object_list=object_list, **kwargs)
+        context = super(OnStudentsFromTutorView, self).get_context_data(object_list=object_list, **kwargs)
         tutor_cabinet = TutorCabinets.objects.get(id=self.kwargs['pk'])
         responses = Response.objects.filter(author_id=tutor_cabinet.user.pk)
         context['responses'] = responses
+        context['chat_page_1'] = '1'
         return context
 
 
@@ -25,9 +26,7 @@ class OnTutorFromStudentResponsesView(LoginRequiredMixin, ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(OnTutorFromStudentResponsesView, self).get_context_data(object_list=object_list, **kwargs)
         user = Account.objects.get(id=self.kwargs['pk'])
-        print(user)
-        # tutor_cabinet = TutorCabinets.objects.get(id=self.kwargs['pk'])
         responses = Response.objects.filter(cabinet_tutor_id=user.tutor.pk)
         context['responses'] = responses
-        context['chat_page'] = '1'
+        context['chat_page_2'] = '1'
         return context
