6860d88587142fabc6fb4e80e042610cf3b71101
Mosunov Anton <antonmossunov@gmail.com>
2023-02-25 01:32:11 +0600

    Рефакторинг представления для отображения главной страницы

--- enimi/views.py
+++ enimi/views.py
@@ -15,21 +15,9 @@ class IndexView(TemplateView):
         review_context = [review for review in reviews]
         review_context = review_context[0:4]
 
-        # tutors = Account.objects.filter()
-        # popular_reviews = Review.objects.all().order_by('-rate')
-        # popular_review_context = [review for review in popular_reviews]
-        # popular_review_context = popular_review_context[0:4]
-
-        # popular_tutors = Account.objects.annotate(
-        #     num_students=Count('students')
-        # ).filter(
-        #     Exists(MyStudent.objects.filter(student=OuterRef('pk')))
-        # ).order_by('-num_students')
-        # print(popular_tutors)
-
         popular_tutors = Account.objects.filter(type='tutor').annotate(num_students=Count('students'))\
             .order_by('-num_students')[0:3]
-        print(popular_tutors)
+
         context['popular_tutors'] = popular_tutors
         context['reviews'] = review_context
         context['types'] = UserCategoryChoices.choices
